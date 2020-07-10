058876ad6a5fc81c1c7984084faadaf496f14cfa
haileyhansard <haileyhansard@gmail.com>
2020-07-10 08:57:50 -0700

    Component mounts plant data into plant array, loads onto browser

--- client/src/components/PlantList.js
+++ client/src/components/PlantList.js
@@ -9,6 +9,20 @@ export default class PlantList extends Component {
   
   // when the component mounts:
   //   - fetch data from the server endpoint - http://localhost:3333/plants
+
+  componentDidMount() {
+    axios 
+    .get("http://localhost:3333/plants")
+    .then(res => {
+      console.log("Response", res.data.plantsData)
+      this.setState({
+        ...this.state, plants: res.data.plantsData
+      })
+    })
+    .catch(err => {
+      console.log("Something went wrong", err)
+    });
+  }
   //   - set the returned plants array to this.state.plants
 
   /*********  DON'T CHANGE ANYTHING IN THE RENDER FUNCTION *********/
