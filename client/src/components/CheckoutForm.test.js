ccf200aef3adca6b26423f4b3aa95f30662c2788
haileyhansard <haileyhansard@gmail.com>
2020-07-10 11:04:41 -0700

    Second test passes, submit button fires

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
@@ -10,4 +10,8 @@ test("form header renders", () => {
     expect(header).toBeInTheDocument()
 });
 
-test("form shows success message on submit with form details", () => {});
+test("form shows success message on submit with form details", () => {
+    render(<CheckoutForm />);
+    const submitButton = screen.getByRole('button');
+    fireEvent.click(submitButton);
+});
